2ba64d4925dc4ea57c5082803e677e047f0de382
unknown <ruhul121980@gmail.com>
2024-05-13 21:04:48 +0600

    fixing error

--- src/components/Services/Service To Do/CardServiceToDo.jsx
+++ src/components/Services/Service To Do/CardServiceToDo.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 export default function CardServiceToDo({ data }) {
   const {
@@ -11,34 +11,32 @@ export default function CardServiceToDo({ data }) {
     displayName,
     providerImg,
     service_date,
-    serviceStatus,
     current_user_name,
   } = data;
+
+  const [serviceStatus, setServiceStatus] = useState(data.serviceStatus);
+
   const handleStatusChange = (e) => {
     const newStatus = e.target.value;
     console.log("now", newStatus);
-    const formData={newStatus}
 
     fetch(`http://localhost:5000/providerBookedService2/${_id}`, {
-        method: 'PATCH',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(formData)
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ newStatus })
     })
     .then(res => res.json())
     .then(data => {
-        
-        
-       
-    setTimeout(function(){
-        location.reload();
-    }, 1000);
-});
-
-    
-    
-  
+      console.log(data);
+     
+      setServiceStatus(newStatus);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
 
   return (
     <div className="card w-96 glass">
@@ -50,14 +48,14 @@ export default function CardServiceToDo({ data }) {
         <p>{description}</p>
         <p>Price: {price}</p>
         <p>Service Area: {service_area}</p>
-        <p>Booked By:{current_user_name}</p>
-        <p className="text-bold"> Appointment Date: {service_date}</p>
-        <p className="text-bold"> Service Status: {serviceStatus}</p>
+        <p>Booked By: {current_user_name}</p>
+        <p className="text-bold">Appointment Date: {service_date}</p>
+        <p className="text-bold">Service Status: {serviceStatus}</p>
         <select value={serviceStatus} onChange={handleStatusChange}>
-        <option value="pending">Pending</option>
-        <option value="working">Working</option>
-        <option value="completed">Completed</option>
-      </select>
+          <option value="pending">Pending</option>
+          <option value="working">Working</option>
+          <option value="completed">Completed</option>
+        </select>
       </div>
     </div>
   );
